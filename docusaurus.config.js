550e35f647aa0e04fa87dd22eda9ae7028602b41
dev-pengi <siffffdoul@gmail.com>
2023-04-02 07:31:41 +0100

    removed the logo icon and updated the description

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Utils Core',
-  tagline: 'save your time, enhance you productivity',
-  favicon: 'img/favicon.ico',
+  tagline: 'a powerful JavaScript utility library that provides a wide range of functions to simplify common programming tasks',
 
   // Set the production url of your site here
   url: 'https://utils-core.js.org',
@@ -89,10 +88,6 @@ const config = {
       // Replace with your project's social card
       navbar: {
         title: 'Utils core',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
         items: [
           {
             type: 'docSidebar',
